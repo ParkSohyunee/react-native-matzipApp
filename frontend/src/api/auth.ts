9e69fae81e5dad37ff99d0fe4d1b8d3da924ce7e
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-09-23 21:42:50 +0900

    feat: auth 관련 api 추가

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,61 @@
+import axiosInstance from './axios';
+import {getEncryptStorage} from '../utils';
+import {Category, Profile} from '../types/domain';
+
+type RequestUser = {
+  email: string;
+  password: string;
+};
+
+const postSignup = async ({email, password}: RequestUser): Promise<void> => {
+  const response = await axiosInstance.post('/auth/signup', {
+    email,
+    password,
+  });
+
+  return response.data;
+};
+
+type ResponseToken = {
+  accessToken: string;
+  refreshToken: string;
+};
+
+const postLogin = async ({
+  email,
+  password,
+}: RequestUser): Promise<ResponseToken> => {
+  const response = await axiosInstance.post('/auth/signin', {
+    email,
+    password,
+  });
+
+  return response.data;
+};
+
+type ResponseProfile = Profile & Category;
+
+const getProfile = async (): Promise<ResponseProfile> => {
+  const response = await axiosInstance.get('/auth/me');
+
+  return response.data;
+};
+
+const getAccessToken = async (): Promise<ResponseToken> => {
+  const refreshToken = await getEncryptStorage('refreshToken');
+
+  const response = await axiosInstance.get('/auth/refresh', {
+    headers: {
+      Authorization: `Bearer ${refreshToken}`,
+    },
+  });
+
+  return response.data;
+};
+
+const logout = async (): Promise<void> => {
+  await axiosInstance.post('/auth/logout');
+};
+
+export {postSignup, postLogin, getProfile, getAccessToken, logout};
+export type {RequestUser, ResponseToken, ResponseProfile};
