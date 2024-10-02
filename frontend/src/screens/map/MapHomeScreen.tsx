f475c1c6859825eb0a9bada3936a1193416efccc
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-10-02 17:29:46 +0900

    feat: 구글 맵으로 현재 위치 보여주기 (react-native-community/geolocation 라이브러리로 좌표 구하기)

--- frontend/src/screens/map/MapHomeScreen.tsx
+++ frontend/src/screens/map/MapHomeScreen.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import {Pressable, StyleSheet, Text} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {DrawerNavigationProp} from '@react-navigation/drawer';
 import {CompositeNavigationProp, useNavigation} from '@react-navigation/native';
 
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {LatLng, PROVIDER_GOOGLE} from 'react-native-maps';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 import {MainDrawerParamListType} from '@/components/navigations/drawer/DrawerMenuNavigator';
 import {MapStackParamListType} from '@/components/navigations/stack/MapStackNavigator';
 import {colors} from '@/constants';
+import Geolocation from '@react-native-community/geolocation';
 
 type Navigation = CompositeNavigationProp<
   StackNavigationProp<MapStackParamListType>,
@@ -19,10 +20,49 @@ type Navigation = CompositeNavigationProp<
 export default function MapHomeScreen() {
   const inset = useSafeAreaInsets();
   const navigation = useNavigation<Navigation>();
+  const [userLocation, setUserLocation] = useState<LatLng>({
+    latitude: 37.5516032365118,
+    longitude: 126.98989626020192,
+  });
+  const [isUserLocationError, setIsUserLocationError] = useState(false);
+  const mapRef = useRef<MapView | null>(null);
+
+  const handlePressUserLocation = () => {
+    // 사용자가 위치 권한을 거부한 경우 등 에러가 발생하는 경우
+    if (isUserLocationError) {
+      // 에러메세지 표시
+      return;
+    }
+
+    mapRef.current?.animateToRegion({
+      latitude: userLocation?.latitude,
+      longitude: userLocation?.longitude,
+      longitudeDelta: 0.0922,
+      latitudeDelta: 0.0421,
+    });
+  };
+
+  // 현재 위치를 구하기
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      info => {
+        const {latitude, longitude} = info.coords;
+        console.log(info, latitude, longitude);
+        setUserLocation({latitude, longitude});
+      },
+      () => {
+        setIsUserLocationError(true);
+      },
+      {
+        enableHighAccuracy: true, // GPS 사용 여부, false면 wifi location
+      },
+    );
+  }, []);
 
   return (
     <>
       <MapView
+        ref={mapRef}
         style={styles.container}
         provider={PROVIDER_GOOGLE}
         showsUserLocation
@@ -33,6 +73,11 @@ export default function MapHomeScreen() {
         onPress={() => navigation.openDrawer()}>
         <Text>서랍</Text>
       </Pressable>
+      <View style={styles.buttonList}>
+        <Pressable style={styles.mapButton} onPress={handlePressUserLocation}>
+          <Text>내 위치</Text>
+        </Pressable>
+      </View>
     </>
   );
 }
@@ -56,5 +101,25 @@ const styles = StyleSheet.create({
 
     borderTopRightRadius: 50,
     borderBottomRightRadius: 50,
+  },
+  buttonList: {
+    position: 'absolute',
+    bottom: 30,
+    right: 1,
+  },
+  mapButton: {
+    backgroundColor: colors.PINK_700,
+    shadowColor: colors.BLACK,
+    shadowOffset: {width: 1, height: 2},
+    shadowOpacity: 0.5,
+    elevation: 4, // 안드로이드 shadow 속성 적용하는 방법
+
+    marginVertical: 5,
+    height: 48,
+    width: 48,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
   },
 });
